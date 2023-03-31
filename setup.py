394a023a3457cb4309464e367bb4df10992c5c55
autodidact24 <tomarshubham24@gmail.com>
2023-03-31 14:41:48 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='pipeline for ML experiments to classify Sign Language images',
+    author='shubham',
+    license='MIT',
+)
